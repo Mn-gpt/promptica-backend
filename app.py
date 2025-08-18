4798c31b0941fc5f8bde3676889304e710c187f7
promptica-project <m.mibrahim1@gmail.com>
2025-08-18 03:38:05 +0300

    Update app.py
    ngrok

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from pyngrok import ngrok
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Server via GitHub Actions + ngrok"
+    return "✅ السيرفر شغال بنجاح مع ngrok!"
 
 if __name__ == "__main__":
-    public_url = ngrok.connect(8000)
-    print("Public URL:", public_url)
-    app.run(host="0.0.0.0", port=8000)
+    app.run(port=5000)
